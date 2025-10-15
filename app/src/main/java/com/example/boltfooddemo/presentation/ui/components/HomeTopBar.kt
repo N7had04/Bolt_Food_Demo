2b71097a5395f836123c7b38c901968cc3193bf5
N7had04 <nihadaxundzade04@gmail.com>
2025-10-15 23:42:00 +0400

    Search Bar visibility while scrolling is fixed

---
+++ app/src/main/java/com/example/boltfooddemo/presentation/ui/components/HomeTopBar.kt
@@ -0,0 +1,68 @@
+package com.example.boltfooddemo.presentation.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Tune
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.example.boltfooddemo.presentation.ui.theme.Green217
+import com.example.boltfooddemo.presentation.ui.theme.LightGray
+
+@Composable
+fun HomeTopBar(
+    isScrollingUp: Boolean,
+) {
+    AnimatedVisibility(
+        visible = isScrollingUp
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(
+                    text = "Food, restaurants, stores...",
+                    color = Color.Gray
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {}
+            ),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(imageVector = Icons.Default.Tune, contentDescription = null)
+                }
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Green217,
+                unfocusedBorderColor = LightGray,
+                cursorColor = Green217,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = LightGray
+            ),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 20.dp)
+        )
+    }
+}
--- app/src/main/java/com/example/boltfooddemo/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/boltfooddemo/presentation/ui/screens/HomeScreen.kt
@@ -17,21 +17,23 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,15 +41,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.boltfooddemo.R
 import com.example.boltfooddemo.data.model.Restaurant
-import com.example.boltfooddemo.presentation.ui.theme.Green217
-import com.example.boltfooddemo.presentation.ui.theme.LightGray
+import com.example.boltfooddemo.presentation.ui.components.HomeTopBar
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -60,103 +60,101 @@ fun HomeScreen(
     onNavigateToInfoScreen: () -> Unit,
     onInsertOrDelete: (Restaurant) -> Unit
 ) {
-    Column(modifier = modifier.fillMaxSize()) {
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            placeholder = {
-                Text(
-                    text = "Food, restaurants, stores...",
-                    color = Color.Gray
-                )
-            },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {}
-            ),
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = null)
-            },
-            trailingIcon = {
-                IconButton(
-                    onClick = {}
-                ) {
-                    Icon(imageVector = Icons.Default.Tune, contentDescription = null)
+    var isScrollingUp by remember { mutableStateOf(true) }
+
+    Scaffold(
+        topBar = { HomeTopBar(isScrollingUp = isScrollingUp) },
+        modifier = modifier.fillMaxSize()
+    ) { padding ->
+
+        val listState = rememberLazyListState()
+
+        LaunchedEffect(listState) {
+            var previousIndex = listState.firstVisibleItemIndex
+            var previousScrollOffset = listState.firstVisibleItemScrollOffset
+
+            snapshotFlow { listState.firstVisibleItemIndex to listState.firstVisibleItemScrollOffset }
+                .collect { (index, offset) ->
+                    isScrollingUp = when {
+                        index < previousIndex -> true
+                        index > previousIndex -> false
+                        offset < previousScrollOffset -> true
+                        offset > previousScrollOffset -> false
+                        else -> isScrollingUp
+                    }
+
+                    previousIndex = index
+                    previousScrollOffset = offset
                 }
-            },
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Green217,
-                unfocusedBorderColor = LightGray,
-                cursorColor = Green217,
-                focusedContainerColor = Color.White,
-                unfocusedContainerColor = LightGray
-            ),
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 20.dp)
-        )
+        }
 
-        if (pastOrders.isNotEmpty()) {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(16.dp).clickable { onNavigateToAllScreen() },
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Order Again",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "All",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-
-                    Spacer(modifier = Modifier.width(8.dp))
-
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
-                        contentDescription = null
-                    )
-                }
-            }
-
-            LazyRow(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                items(pastOrders) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.width(130.dp).clickable{ onNavigateToInfoScreen() }
+        LazyColumn(
+            state = listState,
+            modifier = Modifier.fillMaxSize().padding(padding)
+        ) {
+            if (pastOrders.isNotEmpty()) {
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(16.dp).clickable { onNavigateToAllScreen() },
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.bolt_food),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(120.dp)
-                                .padding(bottom = 8.dp)
-                                .clip(RoundedCornerShape(12.dp)),
-                            contentScale = ContentScale.Crop
-                        )
-
                         Text(
-                            text = it.restaurantName,
-                            fontSize = 16.sp,
-                            textAlign = TextAlign.Center,
+                            text = "Order Again",
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
                         )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "All",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+
+                item {
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(pastOrders) {
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.width(130.dp).clickable{ onNavigateToInfoScreen() }
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.bolt_food),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(120.dp)
+                                        .padding(bottom = 8.dp)
+                                        .clip(RoundedCornerShape(12.dp)),
+                                    contentScale = ContentScale.Crop
+                                )
+
+                                Text(
+                                    text = it.restaurantName,
+                                    fontSize = 16.sp,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
                     }
                 }
             }
-        }
 
-        LazyColumn {
             item {
                 Row(
                     modifier = Modifier.fillMaxWidth().padding(top = 16.dp, start = 16.dp, end = 16.dp),
