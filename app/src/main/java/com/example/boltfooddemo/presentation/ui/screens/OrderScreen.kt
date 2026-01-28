36a803d7405a7f304253fe1bd6a8c0069aceb21e
N7had04 <nihadaxundzade04@gmail.com>
2026-01-28 22:05:52 +0400

    OrderScreen composable implemented!

--- app/src/main/java/com/example/boltfooddemo/presentation/ui/screens/OrderScreen.kt
+++ app/src/main/java/com/example/boltfooddemo/presentation/ui/screens/OrderScreen.kt
@@ -1,18 +1,145 @@
 package com.example.boltfooddemo.presentation.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.boltfooddemo.R
+import com.example.boltfooddemo.data.model.Restaurant
+import com.example.boltfooddemo.presentation.ui.theme.LightGray
 
 @Composable
-fun OrderScreen(modifier: Modifier = Modifier) {
+fun OrderScreen(
+    pastOrders: List<Restaurant>,
+    onNavigateToInfoScreen: (Restaurant) -> Unit,
+    modifier: Modifier = Modifier
+) {
     Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(text = "Orders")
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 30.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "My orders",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(top = 90.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .fillMaxSize()
+        ) {
+            items(pastOrders) {
+                Row(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(0.15f)
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(16.dp))
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.bolt_food),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(0.05f))
+
+                    Column(
+                        modifier = Modifier.weight(0.6f)
+                    ) {
+                        Text(
+                            text = it.restaurantName,
+                            fontSize = 20.sp,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+
+                        Spacer(modifier = Modifier.height(4.dp))
+
+                        Text(
+                            text = "Delivered",
+                            fontSize = 16.sp
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(0.05f))
+
+                    Box(
+                        modifier = Modifier
+                            .weight(0.15f)
+                            .aspectRatio(1f)
+                            .clip(CircleShape)
+                            .background(LightGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        IconButton(
+                            onClick = {
+                                onNavigateToInfoScreen(it)
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Refresh,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderScreenPreview() {
+    OrderScreen(
+        pastOrders = listOf(
+            Restaurant(0, "address", "parkingLot", "name", "type"),
+            Restaurant(1, "address", "parkingLot", "name", "type"),
+            Restaurant(2, "address", "parkingLot", "name", "type")
+        ),
+        onNavigateToInfoScreen = {}
+    )
 }
